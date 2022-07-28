7a7cc02cf885844aa5e5197a7eb3349362b0a2a7
adorno-dev <adorno.dev@gmail.com>
2022-07-28 09:52:09 -0300

    Implementado suporte ao tamanho da imagem

--- Gravatar.Tests/GravatarExtensionTests.cs
+++ Gravatar.Tests/GravatarExtensionTests.cs
@@ -5,16 +5,17 @@ public class GravatarExtensionTests
 {
     [TestCategory("Gravatar Tests")]
     [TestMethod("Should validate gravatar extension")]
-    [DataRow(null, false)]
-    [DataRow("", false)]
-    [DataRow(" ", false)]
-    [DataRow("a@a", false)]
-    [DataRow("a@gmail.com.br", false)]
-    [DataRow("adorno.dev@gmail.com", true)]
-    public void ShouldValidateGravatar(string email, bool status)
+    [DataRow(null, 200, false)]
+    [DataRow("", 200, false)]
+    [DataRow(" ", 200, false)]
+    [DataRow("a@a", 200, false)]
+    [DataRow("a@gmail.com.br", 200, false)]
+    [DataRow("adorno.dev@gmail.com", null, true)]
+    [DataRow("adorno.dev@gmail.com", 200, true)]
+    public void ShouldValidateGravatar(string email, int? size, bool status)
     {
-        var result = "https://gravatar.com/avatar/1a651f1e6f55ad106ef8502857805d7e";
+        var result = $"https://gravatar.com/avatar/1a651f1e6f55ad106ef8502857805d7e?s={size ?? 80}";
 
-        Assert.AreEqual(expected: email.ToGravatar() == result, actual: status);
+        Assert.AreEqual(expected: email.ToGravatar(size ?? 80) == result, actual: status);
     }
 }
